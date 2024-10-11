fa8a65d3a8479a5afa3477037cf4faf2cddfb2fe
Alexey Suprun <alexey.suprun.mail@gmail.com>
2024-10-11 16:17:40 +0200

    refactor: rename sanity studio env vars

--- sanity/env.ts
+++ sanity/env.ts
@@ -9,11 +9,11 @@ function assertValue<T>(v: T | undefined, errorMessage: string): T {
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-10-11';
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET',
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_DATASET',
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID',
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID',
 );
