cac793ec3806c8115fa8eea9f9d888b58fed2233
Alexey Suprun <alexey.suprun.mail@gmail.com>
2024-10-14 21:02:21 +0200

    feat: introduce vercel speed insights to track apps performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={'min-h-dvh'}>{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
