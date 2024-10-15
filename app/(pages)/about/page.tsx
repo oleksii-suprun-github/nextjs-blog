5f8017aed7b6f690cc5e18db9897371b49400888
Alexey Suprun <alexey.suprun.mail@gmail.com>
2024-10-15 18:08:49 +0200

    feat: about page dev, stage 1

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,3 +1,10 @@
+import { SITE_BRAND_TITLE_ENDING } from '@/app/constants';
+
+export const metadata = {
+  title: `About Me ${SITE_BRAND_TITLE_ENDING}`,
+  description: '',
+};
+
 function About() {
   return <div>TBD: About Page</div>;
 }
