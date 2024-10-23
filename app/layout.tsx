66f45304de2d918fc65db18db2dda771a7791aa3
Alexey Suprun <alexey.suprun.mail@gmail.com>
2024-10-23 20:21:07 +0200

    feat: add usercentric gdpr banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
+import Script from 'next/script';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html className="scroll-smooth" lang="en">
+      <head>
+        <Script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="1eec873a-07bc-44b1-95bc-9fe996710116"
+          data-blockingmode="auto"
+          type="text/javascript"
+        ></Script>
+      </head>
       <body className={'min-h-dvh text-stone-300'}>
         {children}
         <SpeedInsights />
