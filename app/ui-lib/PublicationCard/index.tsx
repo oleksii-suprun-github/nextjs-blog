0faa7069914613231e0b8155b863d7a256d2f807
Alexey Suprun <alexey.suprun.mail@gmail.com>
2024-10-23 19:35:38 +0200

    feat: publication card redesign

--- app/ui-lib/PublicationCard/index.tsx
+++ app/ui-lib/PublicationCard/index.tsx
@@ -17,7 +17,33 @@ function PublicationCard({
 
   return (
     <Link href={link}>
-      <div className="card image-full shadow-xl">
+      <div className="card card-compact bg-base-100 shadow-xl">
+        <figure className="lxs-sm:h-[200px] md:h-[250px]">
+          <Image
+            width={400}
+            height={400}
+            src={image.url}
+            alt={image.alt || title}
+            quality={60}
+            placeholder="blur"
+            blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkmHOiHgAD7QHlxT90/wAAAABJRU5ErkJggg=="
+            priority
+            className="h-full w-full object-cover"
+          />
+        </figure>
+        <div className="card-body sm:h-[340px] md:h-[320px] lg:h-[265px]">
+          <span className="text-md text-slate-300">{creationDate}</span>
+          <h2 className="card-title mb-4 text-stone-200">{title}</h2>
+          <p className="text-stone-200">{previewText}</p>
+          <div className="card-actions justify-end">
+            <button className="btn no-animation btn-sm bg-brand-dark-purple text-gray-300 hover:bg-brand-purple">
+              Read More
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="card image-full shadow-xl">
         <figure className="sm:h-[470px] lxs-sm:h-[380px] md:h-[420px]">
           <Image
             width={400}
@@ -41,7 +67,7 @@ function PublicationCard({
             </button>
           </div>
         </div>
-      </div>
+      </div> */}
     </Link>
   );
 }
